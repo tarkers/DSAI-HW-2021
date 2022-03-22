cc802526c90b982f0795c682a368219cb20288fc
Tarkers <anime1314520@gmail.com>
2022-03-22 17:20:24 +0800

    add Data and set simple predict

--- app.py
+++ app.py
@@ -1,11 +1,83 @@
 
+from turtle import color
 from pytest import console_main
 import requests
 from fake_useragent import UserAgent
-user_agent = UserAgent()
-headers = {'Usesr-Agent': 'Mozilla/5.0 (iPad; CPU OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1',
-           "Content-Type": "application/json;charset=UTF-8"}
+import pandas as pd
+from bs4 import BeautifulSoup as BS
+import csv
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 
+plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
+plt.rcParams['axes.unicode_minus'] = False
+def get_electic_data():
+    url = 'https://data.taipower.com.tw/opendata/apply/file/d006005/台灣電力公司_過去電力供需資訊.csv'
+    r = requests.get(url, allow_redirects=True)
+    open('./本年度每日尖峰備轉容量率.csv', 'wb').write(r.content)
+
+
+def modify_data():
+    data = pd.read_csv("./Data/台灣電力公司_過去電力供需資訊.csv", encoding='utf-8')
+    fc = data['日期'].tolist()
+    ee = data['淨尖峰供電能力(MW)'].tolist()
+    dd = data['尖峰負載(MW)'].tolist()
+    ff = data['備轉容量(MW)'].tolist()
+    qq = data['備轉容量率(%)'].tolist()
+    header = ['日期', '淨尖峰供電能力(MW)', '尖峰負載(MW)', '備轉容量(MW)', '備轉容量率(%)', '星期']
+    first = 6
+    with open('./Data/台灣電力公司_過去電力供需整理資訊.csv', 'w', encoding='UTF8', newline='') as f:
+        # create the csv writer
+        writer = csv.writer(f)
+        writer.writerow(header)
+        for i in range(len(fc)):
+            tmp = [fc[i], ee[i], dd[i], ff[i], qq[i], first]
+            first += 1
+            if first > 7:
+                first = 1
+            writer.writerow(tmp)
+
+
+def split_to_date_stage():
+    data = pd.read_csv("./Data/台灣電力公司_過去電力供需整理資訊.csv",
+                       encoding='utf-8', dtype=object)
+    stage_list = {}
+    date_key = ['1', '2', '3', '4', '5', '6', '7']
+    for i in range(7):
+        stage_list[str(i+1)] = []
+    for i in range(len(data)):
+        item = data.loc[i]
+        stage_list[item['星期']].append(
+            [item['淨尖峰供電能力(MW)'], item['尖峰負載(MW)'], item['備轉容量(MW)'],item['日期'],item['日期'][4:-2]])
+    header = ['淨尖峰供電能力(MW)', '尖峰負載(MW)', '備轉容量(MW)','日期','月份']
+    for i in date_key:
+        with open('./Data/台灣電力公司_過去電力供需整理星期{}資訊.csv'.format(i), 'w', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+            for data in stage_list[i]:
+                writer.writerow(data)
+
+def predict_test(date):
+    data = pd.read_csv("./Data/台灣電力公司_過去電力供需整理資訊.csv",
+                       encoding='utf-8', dtype=object)
+    for i in range(len(data)):
+        item = data.loc[i]
+        if item['日期'] == date:
+            return   item['備轉容量(MW)']  
+    return "3000"
+
+def plot_data(day=1):
+    data = pd.read_csv("./Data/台灣電力公司_過去電力供需整理星期{}資訊.csv".format(day), encoding='utf-8', dtype=object)
+    ee = list(map(int, data['淨尖峰供電能力(MW)'].tolist()))
+    dd = list(map(int, data['尖峰負載(MW)'].tolist()))
+    test =list(map(lambda x: x[4:-2],  data['日期']))
+    print(test)
+    plt.plot([*range(len(ee))],ee , label = "淨尖峰供電能力")
+    plt.plot( [*range(len(dd))],dd, label = "尖峰負載",color='red')
+    plt.legend()
+    plt.show()
+# split_to_date_stage()
+# plot_data()
 # You can write code above the if-main block.
 if __name__ == '__main__':
     # You should not modify this part, but additional arguments are allowed.
@@ -47,15 +119,18 @@ if __name__ == '__main__':
                  {"date": '20220412', "day": 2},
                  {"date": '20220413', "day": 3},
                  ]
+    
     with open(args.output, 'w', encoding='UTF8', newline='') as f:
         # create the csv writer
         writer = csv.writer(f)
         writer.writerow(header)
         for date in date_list:
             tmp = []
-            print(date)
+            # print('2021'+date['date'][-4:])
+            # print(predict_test('2021'+date['date'][-4:]))
+            # print(date)
             tmp.append(date['date'])
-            tmp.append("2455")
-            print(tmp)
+            tmp.append(int(predict_test('2021'+date['date'][-4:]))+250)
+            # print(tmp)
             writer.writerow(tmp)
             # write a row to the csv file
